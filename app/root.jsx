c275a68763ddcccf4b2c0b1baed29f2006b47fd1
Josh Chappelow <jdchappelow@gmail.com>
2023-02-14 20:22:27 +0900

    feat: pay button

--- app/root.jsx
+++ app/root.jsx
@@ -10,6 +10,16 @@ import styles from './styles/app.css';
 import favicon from '../public/favicon.svg';
 import {Layout} from './components/Layout';
 import {Seo} from '@shopify/hydrogen';
+import {ShopifyProvider} from '@shopify/hydrogen-react';
+
+const shopifyConfig = {
+  storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+  storeDomain: 'https://hydrogen-preview.myshopify.com',
+  storefrontApiVersion: '2023-01',
+  countryIsoCode: 'US',
+  languageIsoCode: 'en',
+};
+
 
 export const links = () => {
   return [
@@ -38,26 +48,28 @@ export async function loader({context}) {
 
 export default function App() {
   const data = useLoaderData();
-
-  const {name} = data.layout.shop;
+  const {name, description} = data.layout.shop;
 
   return (
-    <html lang="en">
-      <head>
-        <Seo />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Layout title={name}>
-          <Outlet />
-        </Layout>
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
+    <ShopifyProvider {...shopifyConfig}>
+      <html lang="en">
+        <head>
+          <Seo />
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          <Layout title={name}>
+            <Outlet />
+          </Layout>
+          <ScrollRestoration />
+          <Scripts />
+        </body>
+      </html>
+    </ShopifyProvider>
   );
 }
+
 
 const LAYOUT_QUERY = `#graphql
   query layout {
--- app/routes/products/$handle.jsx
+++ app/routes/products/$handle.jsx
@@ -1,7 +1,7 @@
 import {useLoaderData} from '@remix-run/react';
 import {json} from 'react-router';
-import {MediaFile} from '@shopify/hydrogen-react';
 import ProductOptions from '~/components/ProductOptions';
+import {MediaFile, Money, ShopPayButton} from '@shopify/hydrogen-react';
 
 export const loader = async ({params, context, request}) => {
   const {handle} = params;
@@ -36,6 +36,7 @@ export const loader = async ({params, context, request}) => {
 
 export default function ProductHandle() {
   const {product, selectedVariant} = useLoaderData();
+  const orderable = selectedVariant?.availableForSale || false;
 
   return (
     <section className="w-full gap-4 md:gap-8 grid px-6 md:px-8 lg:px-12">
@@ -58,6 +59,20 @@ export default function ProductHandle() {
             options={product.options}
             selectedVariant={selectedVariant}
           />
+          <Money
+            withoutTrailingZeros
+            data={selectedVariant.price}
+            className="text-xl font-semibold mb-2"
+          />
+          {orderable && (
+            <div className="space-y-2">
+              <ShopPayButton
+                variantIds={[selectedVariant?.id]}
+                width={'400px'}
+              />
+              {/* TODO product form */}
+            </div>
+          )}
 
           <div
             className="prose border-t border-gray-200 pt-6 text-black text-md"
