bb94c590050e14c8934f6e26b3fdf9bf58d5d534
Josh Chappelow <jdchappelow@gmail.com>
2023-02-16 20:15:31 +0900

    fix: build revert

--- remix.config.js
+++ remix.config.js
@@ -1,19 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  appDirectory: 'app',
-  ignoredRouteFiles: ['**/.*'],
-  watchPaths: ['./public'],
-  server: './server.js',
-  /**
-   * The following settings are required to deploy Hydrogen apps to Oxygen:
-   */
-  publicPath: (process.env.HYDROGEN_ASSET_BASE_URL ?? '/') + 'build/',
-  assetsBuildDirectory: 'dist/client/build',
-  serverBuildPath: 'dist/worker/index.js',
-  serverMainFields: ['browser', 'module', 'main'],
-  serverConditions: ['worker', process.env.NODE_ENV],
-  serverDependenciesToBundle: 'all',
-  serverModuleFormat: 'esm',
-  serverPlatform: 'neutral',
-  serverMinify: true,
+  ignoredRouteFiles: ["**/.*"],
+  // When running locally in development mode, we use the built-in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  serverBuildPath: "api/index.js",
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
 };
