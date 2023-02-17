f4dbd2bae3b5bf17d5e3b4f10db76807e5450a1d
Josh Chappelow <jdchappelow@gmail.com>
2023-02-17 13:25:53 +0900

    feat: collection title center

--- src/routes/Collections/[handle].server.jsx
+++ src/routes/Collections/[handle].server.jsx
@@ -35,7 +35,7 @@ export default function Collection() {
       <Suspense>
         <Seo type="collection" data={collection} />
       </Suspense>
-      <header className="grid w-full gap-4 p-6 md:p-8 lg:p-12 justify-items-start">
+      <header className="grid w-full gap-4 p-6 md:p-8 lg:p-12 justify-items-center">
         <h1 className="text-4xl whitespace-pre-wrap font-bold inline-block">
           {collection.title}
         </h1>
@@ -52,7 +52,7 @@ export default function Collection() {
       </header>
 
       <section className="w-full gap-4 md:gap-8 grid px-6 md:px-8 lg:px-12">
-        <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mb-12">
           {collection.products.nodes.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
--- src/routes/products.server.jsx
+++ src/routes/products.server.jsx
@@ -27,8 +27,8 @@ export default function All() {
           Jewellery
         </h1>
       </header>
-      <section className="w-full gap-4 md:gap-8 grid px-6 md:px-8 lg:px-12">
-        <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mb-12">
+      <section className="w-full gap-4 md:gap-8 grid px-6 md:px-8 lg:px-12 mb-12">
+        <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mb-3">
           {products.nodes.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
