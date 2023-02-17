7a0d66b0ae63bd045d975f916753cd9c8a886fef
Josh Chappelow <jdchappelow@gmail.com>
2023-02-17 14:33:56 +0900

    fix: correct img routing

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,13 +1,13 @@
 import { Layout } from "../components/Layout.server";
 import { Suspense } from "react";
 import FeaturedCollections from "../components/FeaturedCollections.server";
-import backdrop from '../../public/jewels.jpg';
+// import backdrop from '../../public/jewels.jpg';
 
 export default function Home() {
   return (
     <Layout>
       <div>
-        <img src={backdrop} alt="Fashion" className="opacity-50" />
+        <img src='/assets/jewels.jpg' alt="Fashion" className="opacity-50" />
         <div className="text-center max-w-full text-lg m-auto bg-rose-200 text-white drop-shadow-md rounded-lg p-10 lg:-mt-80 mt-12">
           <p>
             Poly-Jewels provides exquisite accessories for special occassions.
